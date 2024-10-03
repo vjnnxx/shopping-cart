8fd3e40a2e226c884abe232ba4f565f903243d35
vjnnxx <florianovinicius09@gmail.com>
2024-10-03 20:36:59 -0300

    Configuração do ambiente React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navigation from "./Navigation";
+
+function App () {
+  return (
+    <>
+      <Navigation/>
+      <h2>Loja legal :))</h2>
+    </>
+  )
+}
+
+export default App;
---
+++ src/Navigation.jsx
@@ -0,0 +1,11 @@
+function Navigation () {
+    return (
+        <div className="nav">
+            <a href=""> Inicio</a>
+            <a href=""> Produtos</a>
+            <a href=""> Carrinho</a>
+        </div>
+    )
+}
+
+export default Navigation;
